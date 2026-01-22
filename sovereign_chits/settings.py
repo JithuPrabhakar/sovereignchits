26c28e62762acb1bbe6c5d0fba4a90e0c1a31351
JithuPrabhakar <join.jitz@gmail.com>
2026-01-22 22:57:36 +0530

    adds domain names

--- sovereign_chits/settings.py
+++ sovereign_chits/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = 'django-insecure-change-this-in-production-!@#$%^&*()'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["sovereignchits.com.com", "www.sovereignchits.com.com", ".onrender.com"]
+ALLOWED_HOSTS = ["sovereignchits.com", "www.sovereignchits.com", ".onrender.com"]
 
 
 # Application definition
